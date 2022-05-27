0b0e42569607ec4535ee960dc23270731e13242b
rmbass3@gmail.com <rmbass3@gmail.com>
2022-05-27 13:14:01 -0400

    Added responsive buttons

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -6,11 +6,20 @@ import thinking from "../images/thinking.svg"
 
 function Home(props) {
   return (
-    <div className="home d-flex align-items-center">
+    <div className="home d-flex align-items-center" id="home">
       <Container>
-        <Row className="">
+        <Row className="home-row">
           <Col className="home-title-col mt-5 mt-md-0" xs={{ span:12, order:1 }} md={{ span:6, order:12 }}>
-            <h1 className="home-title">Hey, I'm <br/> Ryan Bass</h1>
+            <h1 className="home-title">Hey, I'm <br/> Ryan Bass.</h1>
+            <p className="home-desc">Developer, UNCC Undergraduate</p>
+            <Row className="home-button-row">
+              <Col className="home-contact-col mt-5 mt-md-0" xs={{ span:12, order:12 }} md={{ span:6, order:12 }}>
+                <a href="#contact"><button className="home-contact-button">Contact</button></a>
+              </Col>
+              <Col className="home-about-col mt-3 mt-md-0" xs={{ span:12, order:1 }} md={{ span:6, order:1 }}>
+                <a href="#about"><button className="home-about-button">About</button></a>
+              </Col>
+            </Row>
           </Col>
           <Col className="d-flex align-items-center justify-content-center" xs={{ span:12, order:12 }} md={{ span:6, order:1 }}>
             <img className="thinking-svg" src={thinking} alt="thinking"/>
