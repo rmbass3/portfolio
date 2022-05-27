29a9e0b45953c61a30f44d51ed7736c803469ee4
Ryan <rmbass3@gmail.com>
2022-05-27 18:22:32 -0400

    Added home blob

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -3,15 +3,16 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import thinking from "../images/thinking.svg"
+import homeBlob from "../images/blob2.svg"
 
 function Home(props) {
   return (
     <div className="home d-flex align-items-center" id="home">
       <Container>
         <Row className="home-row">
-          <Col className="home-title-col mt-5 mt-md-0" xs={{ span:12, order:1 }} md={{ span:6, order:12 }}>
+          <Col className="home-title-col mt-3 mt-md-0" xs={{ span:12, order:1 }} md={{ span:6, order:12 }}>
             <h1 className="home-title">Hey, I'm <br/> Ryan Bass.</h1>
-            <p className="home-desc">Developer, UNCC Undergraduate</p>
+            <p className="home-desc mb-3">Developer, UNCC Undergraduate</p>
             <Row className="home-button-row">
               <Col className="home-contact-col mt-5 mt-md-0" xs={{ span:12, order:12 }} md={{ span:6, order:12 }}>
                 <a href="#contact"><button className="home-contact-button">Contact</button></a>
@@ -21,8 +22,9 @@ function Home(props) {
               </Col>
             </Row>
           </Col>
-          <Col className="d-flex align-items-center justify-content-center" xs={{ span:12, order:12 }} md={{ span:6, order:1 }}>
-            <img className="thinking-svg" src={thinking} alt="thinking"/>
+          <Col className="d-flex align-items-center justify-content-center position-relative" xs={{ span:12, order:12 }} md={{ span:6, order:1 }}>
+            <img className="thinking-svg position-absolute" src={thinking} alt="thinking"/>
+            <img className="home-blob" src={homeBlob} alt="blob"/>
           </Col>
         </Row>
       </Container>
