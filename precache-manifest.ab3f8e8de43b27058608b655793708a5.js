00c0e3d34395fa49d4873dc568f341aad6c30ea8
rmbass3@gmail.com <rmbass3@gmail.com>
2022-05-28 20:05:22 -0400

    Updates

---
+++ precache-manifest.ab3f8e8de43b27058608b655793708a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "663931afbb8c038bc2cd",
+    "url": "/portfolio/static/css/main.e996761e.chunk.css"
+  },
+  {
+    "revision": "663931afbb8c038bc2cd",
+    "url": "/portfolio/static/js/main.663931af.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/portfolio/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "29b8f510bd326476c344",
+    "url": "/portfolio/static/css/2.94458453.chunk.css"
+  },
+  {
+    "revision": "29b8f510bd326476c344",
+    "url": "/portfolio/static/js/2.29b8f510.chunk.js"
+  },
+  {
+    "revision": "0f563a0e6fc5d6b56fd5",
+    "url": "/portfolio/static/js/runtime~main.0f563a0e.js"
+  },
+  {
+    "revision": "a33f606f57d7cb056aa0656dff0bcb13",
+    "url": "/portfolio/static/media/squares.a33f606f.svg"
+  },
+  {
+    "revision": "aecef1deda476ca11fa9f757ed24b745",
+    "url": "/portfolio/static/media/thinking.aecef1de.svg"
+  },
+  {
+    "revision": "6e0ac25bd38e065ff785af918139e910",
+    "url": "/portfolio/static/media/blob2.6e0ac25b.svg"
+  },
+  {
+    "revision": "258b6ebfd072a2ce369d2e47cf2182a0",
+    "url": "/portfolio/static/media/rblogo_white.258b6ebf.png"
+  },
+  {
+    "revision": "a46d1d1f81224dd3234187cf2f743b0d",
+    "url": "/portfolio/index.html"
+  }
+];
